4f875bb41a73fcba83e4706b91bcd9579cf399b0
shikhadash <shikha.dash29@gmail.com>
2023-08-21 17:45:05 +0530

    first commit

---
+++ Car.java
@@ -0,0 +1,21 @@
+package Tests;
+
+public class Car extends Vehicle {
+
+	public void start()
+	{
+		System.out.println("Start with key");
+	}
+	Car()
+	{
+		System.out.println("Car constrcutor");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Car c = new Car();
+      c.start();
+	}
+
+}
+.
